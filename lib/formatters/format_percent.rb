1193e71aff0677adb6d3b6cb870b708348d6459e
Brad Folkens <bfolkens@gmail.com>
2006-06-13 00:56:39 +0000

    Added percentage formatting (and fixed a small thing with the phone formatter

--- lib/formatters/format_percent.rb
+++ lib/formatters/format_percent.rb
@@ -2,16 +2,16 @@ require 'rubygems'
 require 'action_view'
 
 module Formatters
-	class FormatPhone < Format
+	class FormatPercent < Format
 		include ActionView::Helpers::NumberHelper
 
 		def from(value)
-			number_to_phone value
+			number_to_percentage value
 		end
 
 		def to(str)
 			return nil if str.nil? or str.empty?
-			str.gsub(/[^0-9]/, '').to_i
+			str.gsub(/[^0-9]/, '').to_f
 		end
 	end
 end
