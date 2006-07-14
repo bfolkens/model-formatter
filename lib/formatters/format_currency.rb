fd18d8597123d1663062167948f27c8a306cd6cc
Brad Folkens <bfolkens@gmail.com>
2006-07-14 02:05:55 +0000

    Should output in integer format

--- lib/formatters/format_currency.rb
+++ lib/formatters/format_currency.rb
@@ -17,7 +17,7 @@ module Formatters
 		def to(str, options = {})
 			return nil if str.nil? or str.empty?
 			val = str.gsub(/[^0-9.]/, '').to_f
-			(val * (10 ** @precision)).to_s unless val.nil?
+			(val * (10 ** @precision)) unless val.nil?
 		end
 	end
 end
